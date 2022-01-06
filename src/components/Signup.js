29b4d78a23e37d207345be08bf561d3cd88536f5
Luiz Felipe <luizfelper@gmail.com>
2022-01-06 11:56:30 -0300

    Criação da tela de login

---
+++ src/components/Signup.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Form, Button, Card } from 'react-bootstrap';
+
+export default function Signup() {
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2>Sign Up</h2>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                Já possui uma conta? Log In
+            </div>
+        </>
+    )
+}
