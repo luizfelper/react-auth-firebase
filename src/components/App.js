e67a9422342224d0cbefa2898f95ca340e661aed
Luiz Felipe <luizfelper@gmail.com>
2022-01-07 09:48:59 -0300

    Inicio configurações das rotas atualizadas do react-router-dom v6

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,22 @@ import React from 'react'
 import Signup from './Signup';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <AuthProvider>
-      <Container className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}>
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}>
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
